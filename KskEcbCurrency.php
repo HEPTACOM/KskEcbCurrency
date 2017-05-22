62815c2ea5350cda51f77bfc862905cae0377a47
Julian Krzefski <krzefski@ksk-agentur.de>
2017-05-23 00:43:18 +0200

    Implemented different ways to update the exchange rates: cron and live.

--- KskEcbCurrency.php
+++ KskEcbCurrency.php
@@ -5,6 +5,7 @@ namespace KskEcbCurrency;
 use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\Tools\ToolsException;
+use Enlight_Controller_Request_Request;
 use Enlight_Event_EventArgs;
 use KskEcbCurrency\Models\InternalStorage;
 use KskEcbCurrency\Services\EcbConnector;
@@ -12,9 +13,14 @@ use Shopware\Components\Model\ModelManager;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\InstallContext;
 use Shopware\Components\Plugin\Context\UpdateContext;
+use Shopware\Components\Plugin\DBALConfigReader;
 
 class KskEcbCurrency extends Plugin
 {
+    const UPDATE_STRATEGY_LIVE = 'live';
+
+    const UPDATE_STRATEGY_CRON = 'cron';
+
     public function install(InstallContext $context)
     {
         $this->updateModels();
@@ -50,10 +56,49 @@ class KskEcbCurrency extends Plugin
     {
         return [
             'Shopware_CronJob_KskEcbCurrency_UpdateEurofxref' => 'executeCronjob',
+            'Enlight_Controller_Front_RouteShutdown' => 'executeLiveUpdate',
         ];
     }
 
     public function executeCronjob(Enlight_Event_EventArgs $args)
+    {
+        if ($this->getUpdateStrategy() !== static::UPDATE_STRATEGY_CRON) {
+            return;
+        }
+
+        $this->doUpdate();
+    }
+
+    public function executeLiveUpdate(Enlight_Event_EventArgs $args)
+    {
+        /** @var Enlight_Controller_Request_Request $request */
+        $request = $args->get('request');
+
+        if ($request->getModuleName() !== 'frontend') {
+            return;
+        }
+
+        if ($this->getUpdateStrategy() !== static::UPDATE_STRATEGY_LIVE) {
+            return;
+        }
+
+        $this->doUpdate();
+    }
+
+    private function getUpdateStrategy()
+    {
+        /** @var DBALConfigReader $configReader */
+        $configReader = $this->container->get('shopware.plugin.config_reader');
+        $config = $configReader->getByPluginName($this->getName());
+
+        if ($config['update_strategy'] === static::UPDATE_STRATEGY_CRON) {
+            return static::UPDATE_STRATEGY_CRON;
+        } else {
+            return static::UPDATE_STRATEGY_LIVE;
+        }
+    }
+
+    private function doUpdate()
     {
         /** @var EcbConnector $connector */
         $connector = $this->container->get('ksk_ecb_currency.services.ecb_connector');
