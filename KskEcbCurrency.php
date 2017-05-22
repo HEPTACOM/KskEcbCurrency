7e1a27db10dea007ec6e7c5eaba0a0380e174e43
Julian Krzefski <krzefski@ksk-agentur.de>
2017-05-23 00:02:19 +0200

    Added basics for a cronjob implementation.

--- KskEcbCurrency.php
+++ KskEcbCurrency.php
@@ -2,9 +2,17 @@
 
 namespace KskEcbCurrency;
 
+use Enlight_Event_EventArgs;
+use KskEcbCurrency\Services\EcbConnector;
 use Shopware\Components\Plugin;
 
 class KskEcbCurrency extends Plugin
 {
 
+    public function executeCronjob(Enlight_Event_EventArgs $args)
+    {
+        /** @var EcbConnector $connector */
+        $connector = $this->container->get('ksk_ecb_currency.services.ecb_connector');
+        $connector->fetch()->apply();
+    }
 }
