e1675afffbce2be806f55c039b69c5b0ddcecf43
Julian Krzefski <krzefski@ksk-agentur.de>
2017-05-23 00:03:50 +0200

    Added new model to have access to some key-value-storage with no other purpose.

--- KskEcbCurrency.php
+++ KskEcbCurrency.php
@@ -2,12 +2,56 @@
 
 namespace KskEcbCurrency;
 
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\ToolsException;
 use Enlight_Event_EventArgs;
+use KskEcbCurrency\Models\InternalStorage;
 use KskEcbCurrency\Services\EcbConnector;
+use Shopware\Components\Model\ModelManager;
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UpdateContext;
 
 class KskEcbCurrency extends Plugin
 {
+    public function install(InstallContext $context)
+    {
+        $this->updateModels();
+    }
+
+    public function update(UpdateContext $context)
+    {
+        $this->updateModels();
+    }
+
+    private function updateModels()
+    {
+        /** @var ModelManager $models */
+        $models = $this->container->get('models');
+        $tool = new SchemaTool($models);
+
+        $schema = [
+            $models->getClassMetadata(InternalStorage::class),
+        ];
+
+        try {
+            $tool->createSchema($schema);
+        } catch (ToolsException $exception) {
+            try {
+                $tool->updateSchema($schema, true);
+            } catch (DBALException $exception) {
+                // TODO handle exception
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware_CronJob_KskEcbCurrency_UpdateEurofxref' => 'executeCronjob',
+        ];
+    }
 
     public function executeCronjob(Enlight_Event_EventArgs $args)
     {
---
+++ Models/InternalStorage.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace KskEcbCurrency\Models;
+
+use Doctrine\ORM\Mapping as ORM;
+use Shopware\Components\Model\ModelEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ksk_ecb_currency_internal_storage")
+ */
+class InternalStorage extends ModelEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $value;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
